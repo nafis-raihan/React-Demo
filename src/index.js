d5b8e6c1f1bb3542a6f3e84385919425247b00be
Nafis <nafis.raihan@northsouth.edu>
2021-05-10 23:26:38 +0600

    new

--- src/index.js
+++ src/index.js
@@ -6,17 +6,11 @@ const date = new Date();
 const currentYear = date.getFullYear();
 
 
-const headingStyle = {
-  backgroundColor : "Purple",
-  Color : "White",
-  textAlign : "center",
-  padding : "15px",
-  fontSize : "4rem"
-}
+
 
 ReactDOM.render(
   <div>
- <h1 style={headingStyle}>Todo App</h1>
+ <h1 className="headingStyle">Todo App</h1>
  <h2>{todoTitle} </h2>
   <p>{currentYear}</p>
   </div>, 
