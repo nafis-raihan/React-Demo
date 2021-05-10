223853db5c9d81edab568ed92c6ba038f5acba15
Nafis <nafis.raihan@northsouth.edu>
2021-05-10 23:12:57 +0600

    fourth Commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const todoTitle = "Call family";
+const date = new Date();
+const currentYear = date.getFullYear();
+
 
 ReactDOM.render(
+  <div>
+ <h1 style={{color : "red", fontSize : "3rem"}}>Todo App</h1>
+ <h2>{todoTitle} </h2>
+  <p>{currentYear}</p>
+  </div>, 
   
-  "Hello World",
+  
   document.getElementById('root')
 );
 
+ 
