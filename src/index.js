dc5be8ff70b67f115758f3b1246052eeab0c6426
Nafis <nafis.raihan@northsouth.edu>
2021-05-12 16:06:50 +0600

    new

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const todoTitle = "Learning and Creating App with React.js";
 const todoName = "Md. Nafis Raihan"
 const group = "Bsc. in Computer Science and Engineering"
 const date = new Date();
+const dateName = date.getDate();
+const monthName = date.getMonth();
 const currentYear = date.getFullYear();
 
 
@@ -17,7 +19,7 @@ ReactDOM.render(
  <h2 className="titleStyle">{todoTitle} </h2>
  <h3 className="nameStyle">{todoName}</h3>
  <h4 className="groupStyle">{group}</h4>
-  <p className="yearStyle">{currentYear}</p>
+  <p className="yearStyle">{dateName + "/" + monthName + "/" + currentYear}</p>
  </div>
  
   </div>, 
