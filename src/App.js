caf494a36ab2eeacdd38b5d3980280e9d1f46182
Nafis <nafis.raihan@northsouth.edu>
2021-05-12 22:24:34 +0600

    json added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Card from './components/card'
+import Data from './data.json'
 
 function App(){
 
     return <div>
         <h1 className="headingStyle">Demo App</h1>
-        <Card name="Md. Nafis Raihan" address="Azimpur" />
+        <Card name={Data[0].name} address={Data[0].address} />
         <Card name="Tanjil Jishad" address="Gaibandha"/>
         <Card name="Ishtiaque Hossain Ayon" address="South Banasree"/>
     </div>
