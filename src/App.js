d64c6dfb7e0794654e905a81c0652743932c779f
Nafis <nafis.raihan@northsouth.edu>
2021-05-12 22:58:25 +0600

    Class Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Card from './components/card'
+import Card2 from './components/card2'
 import Data from './data.json'
 
 function App(){
@@ -9,6 +10,7 @@ function App(){
         <Card name={Data[0].name} address={Data[0].address} />
         <Card name="Tanjil Jishad" address="Gaibandha"/>
         <Card name="Ishtiaque Hossain Ayon" address="South Banasree"/>
+        <Card2 />
     </div>
 }
 
---
+++ src/components/card2.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Card2 extends Component{
+
+    render() {
+
+        return <div>
+            <h3 style={{textAlign : "center", marginTop : "20px"}}>Hello this is Class Component</h3>
+        </div>
+        
+    }
+}
+
+export default Card2;
